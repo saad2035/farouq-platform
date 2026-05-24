ac232d3aeffdd716853ec33537393bc192142259
Saad <shalthobiani@hotmail.com>
2026-05-24 23:44:42 +0300

    force clean homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-"use client"
-
-import CommitteeList from "./components/CommitteeList"
-
 export default function Home() {
-  return <CommitteeList />
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#020817",
+        color: "white",
+        padding: "40px",
+      }}
+    >
+      <h1 style={{ fontSize: "50px" }}>
+        منصة الفاروق الثانوية
+      </h1>
+
+      <p>نجح الاتصال بالصفحة الرئيسية</p>
+    </main>
+  )
 }
